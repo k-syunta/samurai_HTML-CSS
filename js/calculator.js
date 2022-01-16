7150c28665c024e53d3b5f925caac562eb36ed0c
k-syunta <95166869+k-syunta@users.noreply.github.com>
2022-01-16 17:39:53 +0900

    途中経過

---
+++ js/calculator.js
@@ -0,0 +1,171 @@
+'use strict';
+
+//変数の定義
+let answer = "";
+
+let clearflag = false;  //数字の有無
+let enzan = "";  //演算記号の初期は空
+let dot = false;  //小数点の有無
+let errorflag = false;  //エラーフラグ
+let cflag = false;  //AC,Cの切り替え
+
+//ACを押すと値がクリアされる
+$(function (){
+  $('#clear').click(function (){
+    //cflag = true　ACがCになっていた時の場合
+    if(cflag == true) {
+      $('#answer').val("");
+      cflag = false;
+      $('#clear').val('AC');
+      $('.btn').prop('disabled', false);
+      console.log(answer);
+    //cflag == false　ACの場合
+    } else if(cflag == false) {
+      $('#answer').val("");
+      answer = "";
+      $('#clear').val('AC');
+      //演算記号もクリアに
+      enzan = "";
+      $('#dot').prop('disabled', false);
+      $('.btn').prop('disabled', false);
+      dot = false;
+      clearflag = false;
+      errorflag = false;
+      console.log(answer);
+    }
+  });
+});
+
+//数字ボタンのクリックでの数値の表示
+$(function (){
+  $('.btn').click(function (){
+    //数値の桁数が１０桁以上の場合
+    if($('#answer').val().length >= 10) {
+      $('.btn').prop('disabled', true);
+      $('#answer').val('error!');
+    } else {
+      //cleaflag = trueの場合表示中の数値をクリアする
+      if(clearflag === true) {
+        $('#answer').val("");
+        //数字を押したときに表示がクリアされるようにクリアフラグを定義
+        clearflag = false;
+        //小数点を押せるようにする
+        $('#dot').prop('disabled', false);
+      }
+    }
+
+    //AC表示をCに切り替える
+    $('#clear').val('C');
+    Number($('#answer').val($('#answer').val() + $(this).val()));
+    cflag = true;
+  });
+});
+
+//小数点が一度押されたら表示中に2度は押せなくする
+$(function (){
+  $('#dot').on('click', function (){
+    $('#dot').prop('disabled', true);
+    Number($('#answer').val($('#answer').val() + $(this).val()));
+  });
+});
+
+//足し算記号のクリックでenzanClickに記号を渡す
+$(function (){
+  $('#addition').click(function (){
+    enzanClick('+');
+  });
+});
+
+//引き算記号のクリックでenzanClickに記号を渡す
+$(function (){
+  $('#substraction').click(function (){
+    enzanClick('-');
+  });
+});
+
+//掛け算記号のクリックでenzanClickに記号を渡す
+$(function (){
+  $('#multiplication').click(function (){
+    enzanClick('×');
+  });
+});
+
+//割り算記号のクリックでenzanClickに記号を渡す
+$(function (){
+  $('#devision').click(function (){
+    enzanClick('÷');
+  });
+});
+
+//%を押したら表示中の数字に0.01をかける
+$(function (){
+  $('#percent').click(function (){
+    $('#answer').val(Number($('#answer').val()) * 0.01);
+  });
+});
+
+//+/-を押したら表示中の数字に(-1)かける
+$(function (){
+  $('#plusminus').click(function (){
+    $('#answer').val(Number($('#answer').val()) * (-1));
+  });
+});
+
+//enzanClickは前の演算記号を覚えてなければ今の表示している値をanswerに保存
+function enzanClick(kigou) {
+  if(enzan === '') {
+    answer = Number($('#answer').val());
+  } else {
+    enzanClick1()
+  }
+  //表示桁数を10までにする
+  digit();
+
+  clearflag = true;
+  enzan = kigou;
+}
+
+//演算記号に何か覚えていれば、answerに保存した値と表示中の値と記憶した演算記号で計算
+function enzanClick1() {
+  if(enzan === '+') {
+    answer += Number($('#answer').val());
+  } else if(enzan === '-') {
+    answer -= Number($('#answer').val());
+  } else if(enzan === '×') {
+    answer *= Number($('#answer').val());
+  } else if(enzan === '÷') {
+    answer /= Number($('#answer').val());
+  }
+}
+
+//=を押したとき
+$(function (){
+  $('#equal').click(function (){
+    enzanClick1();
+    digit();
+
+    clearflag = false;
+    cflag = false;
+    $('#clear').val('AC');
+  });
+});
+
+//表示できる桁数を10桁以内にする
+function digit() {
+  //小数点で切り分ける
+  let num1 = String(answer).split('.');
+
+  //整数部分で11桁以上の場合、エラーを表示
+  if(num1[0].length > 10) {
+    $('#answer').val('error!');
+    errorflag = true;
+    //整数部が10桁までの場合、整数部はそのまま表示、少数部は値を丸めて全部で10桁以内にする
+    //10から整数部の桁を引いた数だけ小数部を表示できる
+  } else if(String(answer).length >= 10 && num1[0].length <= 10) {
+    let n = 10 - num1[0].length;
+    answer = Number(answer).toFixed(n);
+    $('#answer').val(answer);
+  } else {
+    $('#answer').val(answer);
+  }
+}
--- js/shoppingList.js
+++ js/shoppingList.js
@@ -1,1 +1,156 @@
 'use strict';
+
+//リスト内容をローカルストレージに保存する
+let item = document.getElementById('item');
+
+//--------------------------------------------------------------------------------
+
+//プラスボタンのクリックの動作
+const addBtn = document.getElementById('addBtn');
+const mainList = document.getElementById('mainList');
+
+//一番最後にあるinput要素を取得
+let lastChild = mainList.lastElementChild;
+let childInput = lastChild.lastElementChild;
+
+//買い物リストに表示されているものを格納する配列
+//let valueList = [];
+
+addBtn.addEventListener('click', ()=> {
+
+  //ここでもう一度新しい最後の子要素を定義したらいける
+  let lastChild = mainList.lastElementChild;
+  let childInput = lastChild.lastElementChild;
+
+  //最後のinput要素が空欄ならその要素にカーソルをセットする
+  if(childInput.value === '') {
+    setCursor();
+  //最後のinput要素が空欄ではない場合、新しいinput要素を生成しその要素にカーソルをセットする
+  } else {
+    makeList();
+  }
+
+});
+
+function makeList() {
+  //input要素を生成
+  let newInput = document.createElement('input');
+  newInput.id = 'item';
+  newInput.className = 'item';
+  newInput.type = 'text';
+  newInput.name = 'item';
+  newInput.placeholder = 'タップして入力';
+  //li要素を生成
+  let newLi = document.createElement('li');
+  newLi.className = 'listItem';
+  //生成した要素をそれぞれの要素に入れ込んでいく
+  mainList.appendChild(newLi);
+  newLi.appendChild(newInput);
+}
+
+//--------------------------------------------------------------------------------
+
+
+
+//--------------------------------------------------------------------------------
+
+//clickZoneのクリックで一番最後のinput要素(li要素)にカーソルをセットする
+const clickZone = document.getElementById('clickZone');
+
+clickZone.addEventListener('click', ()=> {
+  //input要素が空欄の状態の場合
+  if(childInput.value === '') {
+    setCursor();
+  }
+});
+
+//カーソルを合わせるためのメソッド
+function setCursor() {
+
+  let lastChild = mainList.lastElementChild;
+  let childInput = lastChild.lastElementChild;
+
+  childInput.focus();
+  //input要素の0文字目にカーソルを合わせる
+  childInput.setSelectionRange(0, 0);
+}
+
+//--------------------------------------------------------------------------------
+
+//追加されたものから順にローカルストレージに保存していく
+
+//買い物リストに表示されているものを格納する配列
+let valueList = [];
+
+function keepList() {
+
+  const mainList = document.getElementById('mainList');
+
+  //関数内でもう一度定義する
+  //let lastChild = mainList.lastElementChild;
+  //let childInput = lastChild.lastElementChild;
+
+  let item = document.querySelectorAll('.item');
+
+  for(let i = 0; i < item.length; i++) {
+    console.log(item[i].value);
+    valueList.push(item[i].value);
+  }
+
+  localStorage.setItem("key_valueList", JSON.stringify(valueList));
+
+}
+
+//--------------------------------------------------------------------------------
+
+//保存した買い物リストの内容をリストを開くたびに元の状態に表示するための関数
+function displayList() {
+
+  //もともと表示されている一つ目のリストを非表示にする
+  let listItem = document.getElementById('listItem');
+  listItem.classList.add('nolook');
+
+  let jsonValue = JSON.parse(localStorage.getItem("key_valueList"));
+
+  for(let i = 0; i < jsonValue.length; i++) {
+    let newInput = document.createElement('input');
+    newInput.id = 'item';
+    newInput.className = 'item';
+    newInput.type = 'text';
+    newInput.name = 'item';
+    newInput.value = jsonValue[i];
+    //li要素を生成
+    let newLi = document.createElement('li');
+    newLi.className = 'listItem';
+    //生成した要素をそれぞれの要素に入れ込んでいく
+    if(newInput.value !== '') {
+      mainList.appendChild(newLi);
+      newLi.appendChild(newInput);
+    }
+
+    //もう一度ローカルストレージに保存することによってリストボタンが連続で押されてもリストには追加されない
+    localStorage.setItem("key_valueList", JSON.stringify(valueList));
+  }
+}
+
+//--------------------------------------------------------------------------------
+
+//画面の切り替えをするボタンをクリックするときは買い物リストの内容をローカルストレージに保存するようにする
+const calculatorBtn = document.getElementById('calculatorBtn');
+
+calculatorBtn.addEventListener('click', ()=> {
+  keepList();
+})
+
+//買い物リストを表示するボタンをクリックするときは元の状態に戻せるように関数を定義する
+const shoppingListBtn = document.getElementById('shoppingListBtn');
+
+shoppingListBtn.addEventListener('click', ()=> {
+  displayList();
+})
+
+//--------------------------------------------------------------------------------
+
+//--------------------------------------------------------------------------------
+
+console.log(localStorage);
